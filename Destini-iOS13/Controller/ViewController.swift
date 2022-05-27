b5824c04710ea900cac4b629199412e4a6f50ccf
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2022-05-26 23:06:07 -0400

    Update comments and launch screen

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,10 +1,6 @@
-//
+//  SABMOOTH
 //  ViewController.swift
 //  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,11 +10,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.text = storyBrain.stories[0].title
+        choice1Button.setTitle(storyBrain.stories[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2, for: .normal)
 
     }
 
-
+    @IBAction func choicePressed(_ sender: UIButton) {
+        let storyStep = storyBrain.updateStep(sender.currentTitle!)
+        
+        storyLabel.text = storyBrain.stories[storyStep].title
+        choice1Button.setTitle(storyBrain.stories[storyStep].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyStep].choice2, for: .normal)
+        
+    }
+    
 }
 
--- Destini-iOS13/SceneDelegate.swift
+++ Destini-iOS13/SceneDelegate.swift
@@ -1,8 +1,7 @@
-//
+//  Sabah Naveed
 //  SceneDelegate.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
